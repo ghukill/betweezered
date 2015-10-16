deee2e917a8ccd872a65c8b7dbc19e678c2ac98e
Graham Hukill <ghukill@gmail.com>
2015-10-16 19:09:07 -0400

    air times, created local stream

--- README.md
+++ README.md
@@ -3,4 +3,20 @@ Application / Ecosystem to filter streaming twitter api, capture, parse, and pre
 
 
 ## Installation
-* Do Thing
+* We'll get there...
+
+
+## Run
+* Star consumer: `rqworker normal`
+	** `rqworker normal` must be run from root directory of project 
+* Start listener: `python twitter_capture.py`
+* Start webapp: `python runserver.py`
+
+
+'''
+Sitting here mulling it over, the hose might not be the right approach.
+
+UNLESS, each instance of betweezered can cast a net over a few search terms, 
+these are ALL captured in the same DB.  We could try to guess which term landed it, 
+but maybe we'll not know?  maybe the tweet metadata will include it?  All is not lost.
+'''
--- twitter_capture.py
+++ twitter_capture.py
@@ -1,12 +1,20 @@
 # module to connect to twitter stream
+
+import json
+import time
+import sys
+
 import localConfig
 from twarc import Twarc
 from twisted.internet import protocol
 from twitter_work import worker 
 
+
 class TwitterStream(object):
 
+	# WORKING TWITTER HOSE
 	def __init__(self, search_terms):
+
 		# globals to all instances
 		self.t = Twarc(localConfig.client_key, localConfig.client_secret, localConfig.access_token, localConfig.access_token_secret)
 		self.search_terms = search_terms
@@ -14,13 +22,38 @@ class TwitterStream(object):
 	# method to capture twitter stream
 	def captureStream(self):		
 		for tweet in self.t.stream(",".join(self.search_terms)):
-			worker.delay(tweet)		
+			worker.delay(tweet)
+
+
+
+class LocalStream(object):
+
+	# LOCAL TWITTER HOSE EMULATOR
+	def __init__(self, search_terms):
+
+		# globals to all instances
+		self.search_terms = search_terms
+		lt = open('local_tweets.json','r')
+		self.lt = json.loads(lt.read())
+
+	# method to capture twitter stream
+	def captureStream(self):
+		while True:
+			print "Sending a round!"		
+			for tweet in self.lt:
+				worker.delay(tweet)
+			time.sleep(3)
 
 
 def main():
 
-	ts = TwitterStream(localConfig.search_terms)
-	ts.captureStream()
+	stream_type = sys.argv[1]
+	if stream_type == 'ts':
+		ts = TwitterStream(localConfig.search_terms)
+		ts.captureStream()
+	if stream_type == 'lt':
+		lt = LocalStream(localConfig.search_terms)
+		lt.captureStream()	
 
 
 if __name__ == '__main__':

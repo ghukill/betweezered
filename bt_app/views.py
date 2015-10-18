e47742836859d7e2a0d7fcb9a56f9cca33d85d9a
Graham Hukill <ghukill@gmail.com>
2015-10-18 13:36:34 -0700

    pre pause

--- bt_app/views.py
+++ bt_app/views.py
@@ -10,11 +10,10 @@ from flask import jsonify
 import localConfig
 from bt_app import app, models
 
-# write crumb
+# return json for tweet
 @app.route("{prefix}/tweet/<id>".format(prefix=localConfig.betweezered_app_prefix), methods=['GET', 'POST'])
 def tweet(id):
 
-
+	# return tweet json
 	tweet = models.Tweet.query.filter_by(id=id).first()
-
 	return jsonify(**json.loads(tweet.tweet_json))
